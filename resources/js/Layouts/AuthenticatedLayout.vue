a527fb1c7c8dc5f7e3d2f977c95f53c3fce6ccc9
AnaelBonnafous <anael.bonnafous@gmail.com>
2023-07-28 12:09:03 +0200

    feat(dashboard): add links to celebrities table

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -32,6 +32,9 @@ const showingNavigationDropdown = ref(false);
                                 <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
                                     Dashboard
                                 </NavLink>
+                                <NavLink :href="route('celebrities.index')" :active="route().current()?.startsWith('celebrities')">
+                                    Célébrités
+                                </NavLink>
                             </div>
                         </div>
 
@@ -114,6 +117,9 @@ const showingNavigationDropdown = ref(false);
                     <div class="pt-2 pb-3 space-y-1">
                         <ResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
                             Dashboard
+                        </ResponsiveNavLink>
+                        <ResponsiveNavLink :href="route('celebrities.index')" :active="route().current()?.startsWith('celebrities')">
+                            Célébrités
                         </ResponsiveNavLink>
                     </div>
 
