e86e93c509e06505c27d9059a761021ff1d87497
AnaelBonnafous <anael.bonnafous@gmail.com>
2023-07-28 11:26:39 +0200

    fix(local): set vite_hmr_host for docker environnement

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,29 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig, loadEnv } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.ts',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+
+    return {
+        plugins: [
+            laravel({
+                input: "resources/js/app.ts",
+                refresh: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
                 },
+            }),
+        ],
+        server: {
+            hmr: {
+                host: env.VITE_HMR_HOST || "localhost",
             },
-        }),
-    ],
+        },
+    };
 });
