2fcb4ca422ec979ed207f0b1e931d372c1772bca
AnaelBonnafous <anael.bonnafous@gmail.com>
2023-07-28 11:55:47 +0200

    feat(routes): register celebrities resource controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CelebrityController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -29,6 +30,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
+
+    Route::resource('celebrities', CelebrityController::class);
 });
 
 Route::middleware('auth')->group(function () {
